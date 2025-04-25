1a32769524566a325bfb1dd5f796dfafb424e8c1
Juan Carlos <jcs1011@gmail.com>
2025-04-25 20:01:39 +0200

    se agrega en Layaut collapseOnSelect

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -4,15 +4,15 @@ import { Link } from 'react-router-dom';
 
 const NavBar = () => {
   return (
-    <Navbar expand="lg" bg="light" variant="light">
+    <Navbar collapseOnSelect expand="lg" bg="light" variant="light">
       <Container>
         <Navbar.Brand as={Link} to="/">TiendaOnline</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link as={Link} to="/">Home</Nav.Link>
-            <Nav.Link as={Link} to="/products">Products</Nav.Link>
-            <Nav.Link as={Link} to="/calzadojm">CalzadoJM</Nav.Link>
+            <Nav.Link as={Link} to="/" eventKey="1">Home</Nav.Link>
+            <Nav.Link as={Link} to="/products" eventKey="2">Products</Nav.Link>
+            <Nav.Link as={Link} to="/calzadojm" eventKey="3">CalzadoJM</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
